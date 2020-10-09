b11c56d1f8d8ee2a3011c9450fc3c0d5001a8a8b
srishti <srishtivermachamp@gmail.com>
2020-10-09 17:39:00 +0530

    Updates

---
+++ precache-manifest.cffad00069464d396cafa883f4958bbb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b601401a52dfcb927119837492459b6",
+    "url": "/cartnew/index.html"
+  },
+  {
+    "revision": "49ddde47d428200296e6",
+    "url": "/cartnew/static/css/main.41b83837.chunk.css"
+  },
+  {
+    "revision": "2d6379f77b4fcd7d3454",
+    "url": "/cartnew/static/js/2.c25eac14.chunk.js"
+  },
+  {
+    "revision": "a1a636632721a402612df3e043dd630d",
+    "url": "/cartnew/static/js/2.c25eac14.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "49ddde47d428200296e6",
+    "url": "/cartnew/static/js/main.c43eb0e8.chunk.js"
+  },
+  {
+    "revision": "403ccf95e9dd0258ba88",
+    "url": "/cartnew/static/js/runtime-main.f539040f.js"
+  }
+]);
